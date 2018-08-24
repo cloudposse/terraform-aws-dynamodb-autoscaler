7f0baf6a27f0aeb5265ac1319f55ac9e55fc3b5d
Andriy Knysh <aknysh@users.noreply.github.com>
2018-08-24 11:23:14 -0400

    Use `splat+join` pattern for resources with counts (#10)

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ data "aws_iam_policy_document" "autoscaler" {
 resource "aws_iam_role_policy" "autoscaler" {
   count  = "${var.enabled == "true" ? 1 : 0}"
   name   = "${module.default_label.id}${var.delimiter}autoscaler${var.delimiter}dynamodb"
-  role   = "${aws_iam_role.autoscaler.id}"
+  role   = "${join("", aws_iam_role.autoscaler.*.id)}"
   policy = "${data.aws_iam_policy_document.autoscaler.json}"
 }
 
@@ -73,7 +73,7 @@ data "aws_iam_policy_document" "autoscaler_cloudwatch" {
 resource "aws_iam_role_policy" "autoscaler_cloudwatch" {
   count  = "${var.enabled == "true" ? 1 : 0}"
   name   = "${module.default_label.id}${var.delimiter}autoscaler${var.delimiter}cloudwatch"
-  role   = "${aws_iam_role.autoscaler.id}"
+  role   = "${join("", aws_iam_role.autoscaler.*.id)}"
   policy = "${data.aws_iam_policy_document.autoscaler_cloudwatch.json}"
 }
 
@@ -97,11 +97,11 @@ resource "aws_appautoscaling_target" "read_target_index" {
 
 resource "aws_appautoscaling_policy" "read_policy" {
   count              = "${var.enabled == "true" ? 1 : 0}"
-  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.read_target.resource_id}"
+  name               = "DynamoDBReadCapacityUtilization:${join("", aws_appautoscaling_target.read_target.*.resource_id)}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = "${aws_appautoscaling_target.read_target.resource_id}"
-  scalable_dimension = "${aws_appautoscaling_target.read_target.scalable_dimension}"
-  service_namespace  = "${aws_appautoscaling_target.read_target.service_namespace}"
+  resource_id        = "${join("", aws_appautoscaling_target.read_target.*.resource_id)}"
+  scalable_dimension = "${join("", aws_appautoscaling_target.read_target.*.scalable_dimension)}"
+  service_namespace  = "${join("", aws_appautoscaling_target.read_target.*.service_namespace)}"
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
@@ -149,11 +149,11 @@ resource "aws_appautoscaling_target" "write_target_index" {
 
 resource "aws_appautoscaling_policy" "write_policy" {
   count              = "${var.enabled == "true" ? 1 : 0}"
-  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.write_target.resource_id}"
+  name               = "DynamoDBWriteCapacityUtilization:${join("", aws_appautoscaling_target.write_target.*.resource_id)}"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = "${aws_appautoscaling_target.write_target.resource_id}"
-  scalable_dimension = "${aws_appautoscaling_target.write_target.scalable_dimension}"
-  service_namespace  = "${aws_appautoscaling_target.write_target.service_namespace}"
+  resource_id        = "${join("", aws_appautoscaling_target.write_target.*.resource_id)}"
+  scalable_dimension = "${join("", aws_appautoscaling_target.write_target.*.scalable_dimension)}"
+  service_namespace  = "${join("", aws_appautoscaling_target.write_target.*.service_namespace)}"
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
