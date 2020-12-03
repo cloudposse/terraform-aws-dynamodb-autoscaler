fdb1bc0efd1df73c826c9d667cac06f1b8e27340
Jürgen Weber <jurgen@space-marine.org>
2020-12-03 11:37:12 +1100

    go to v21 which is tf v13 compat

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "default_label" {
-  source      = "git::ssh://git@github.com/jurgenweber/terraform-null-label.git?ref=master"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.21.0"
   namespace   = var.namespace
   stage       = var.stage
   environment = var.environment
