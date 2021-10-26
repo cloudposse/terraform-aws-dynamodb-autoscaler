5c44581a3990c7f7def70449617cd39c4ed67568
nitrocode <nitrocode@users.noreply.github.com>
2021-10-26 10:50:30 -0500

    Add index value vars

--- variables.tf
+++ variables.tf
@@ -49,3 +49,39 @@ variable "autoscale_max_write_capacity" {
   default     = 20
   description = "DynamoDB autoscaling max write capacity"
 }
+
+variable "autoscale_write_target_index" {
+  type        = number
+  default     = null
+  description = "The target value for DynamoDB write autoscaling of the index"
+}
+
+variable "autoscale_read_target_index" {
+  type        = number
+  default     = null
+  description = "The target value for DynamoDB read autoscaling of the index"
+}
+
+variable "autoscale_min_read_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling min read capacity of the index"
+}
+
+variable "autoscale_max_read_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling max read capacity of the index"
+}
+
+variable "autoscale_min_write_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling min write capacity of the index"
+}
+
+variable "autoscale_max_write_capacity_index" {
+  type        = number
+  default     = null
+  description = "DynamoDB autoscaling max write capacity of the index"
+}
