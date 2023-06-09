bf322c2e3c114a7a30834c9dec5ac3259f72fb10
Max Lobur <max_lobur@outlook.com>
2023-06-09 19:50:22 +0300

    Support AWS Provider V5 (#52)
    * Support AWS Provider V5
    
    * Support AWS Provider V5

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "appautoscaling_read_policy_arn" {
-  value       = join("", aws_appautoscaling_policy.read_policy.*.arn)
+  value       = join("", aws_appautoscaling_policy.read_policy[*].arn)
   description = "Appautoscaling read policy ARN"
 }
 
 output "appautoscaling_write_policy_arn" {
-  value       = join("", aws_appautoscaling_policy.write_policy.*.arn)
+  value       = join("", aws_appautoscaling_policy.write_policy[*].arn)
   description = "Appautoscaling write policy ARN"
 }
